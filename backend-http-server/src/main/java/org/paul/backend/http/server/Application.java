8907b05aa663db8475978acf33368a8d77ccb712
paul121388 <2265370493@qq.com>
2024-09-15 23:45:20 +0800

    核心容器启动，http测试成功

---
+++ backend-http-server/src/main/java/org/paul/backend/http/server/Application.java
@@ -0,0 +1,18 @@
+package org.paul.backend.http.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @GetMapping("/http-demo/ping")
+    public String ping() {return "pong";}
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
