501337f8ec35f8921e34f3f0891f45e35f153d3a
2265370493@qq.com <2265370493@qq.com>
2024-09-28 12:52:53 +0800

    修改路由过滤器，当熔断规则不存在时，才进行重试，否则不重试

--- gateway-core/src/main/java/org/paul/core/filter/router/RouterFilter.java
+++ gateway-core/src/main/java/org/paul/core/filter/router/RouterFilter.java
@@ -17,6 +17,7 @@ import org.paul.core.response.GatewayResponse;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeoutException;
 
@@ -33,43 +34,98 @@ import static org.paul.common.constants.FilterConst.*;
 public class RouterFilter implements Filter {
 
     /**
-     * 路由函数
-     * 获取request对象（真正发送时使用
-     * 调用自定义AsyncHttpHelper，首先获取实例，然后执行request，返回Future对象
-     * <p>
-     * 拿到配置：通过configLoader获取config的单双异步配置信息
-     * 如果单异步
-     * 调用future的whencomplete，传入闭包，参数时response，throwable
-     * 封装方法complete，参数：请求，响应，throwable，gatewaycontext上下文
-     * 双异步类似
-     * 闭包逻辑一致，改为异步调用whencomplete异步方法
+     * 执行过滤，处理异步请求结果
+     *
+     * @param gatewayContext
+     * @throws Exception
      */
     @Override
     public void doFilter(GatewayContext gatewayContext) throws Exception {
+        //使用自定义的方法获取rule中的HystrixConfig熔断配置，使用Optional进行包装
+        Optional<Rule.HystrixConfig> hystrixConfig = getHystrixConfig(gatewayContext);
+        //不一定每个接口都要走熔断，首先判断HystrixConfig熔断配置是否存在，存在：走有熔断的方法，不存在：走正常的重试
+        if (hystrixConfig.isPresent()) {
+            routeWithHystrix(gatewayContext, hystrixConfig);
+        } else {
+            route(gatewayContext, hystrixConfig);
+        }
+    }
+
+
+
+
+    /**
+     * 定义route方法，没有熔断配置时，就是原来的路由转发的代码
+     * 在这段代码中还需要再次判断是否有熔断配置，没有才进行重试
+     * @param gatewayContext
+     * @param hystrixConfig
+     */
+    private void route(GatewayContext gatewayContext, Optional<Rule.HystrixConfig> hystrixConfig) {
+        //获取request对象（真正发送给下游服务时使用）
         Request request = gatewayContext.getRequest().build();
+        //调用自定义AsyncHttpHelper，首先获取实例，然后执行request，返回Future对象
         CompletableFuture<Response> future = AsyncHttpHelper.getInstance().executeRequest(request);
 
+        //拿到配置：通过configLoader获取config的单双异步配置信息
         boolean whenComplete = ConfigLoader.getConfig().isWhenComplete();
+
+        //单异步
         if (whenComplete) {
+            //调用future的whencomplete，传入闭包，参数时response，throwable
+            //封装方法complete，参数：请求，响应，throwable，gatewaycontext上下文
+            //在这段代码中还需要再次判断是否有熔断配置，没有才进行重试
             future.whenComplete((response, throwable) -> {
-                complete(request, response, throwable, gatewayContext);
+                complete(request, response, throwable, gatewayContext, hystrixConfig);
             });
+            //双异步
         } else {
+            //调用future的whencomplete，传入闭包，参数时response，throwable
+            //封装方法complete，参数：请求，响应，throwable，gatewaycontext上下文
             future.whenCompleteAsync((response, throwable) -> {
-                complete(request, response, throwable, gatewayContext);
+                complete(request, response, throwable, gatewayContext, hystrixConfig);
             });
         }
     }
 
     /**
-     * complete方法：参数：请求，响应，throwable，gatewayContext上下文
-     * catch
-     * 在gatewayContext中记录信息，打印日志
-     * finally
-     * 修改gatewayContext的状态
-     * 调用辅助类responseHelper，写回数据
+     * 获取熔断配置
+     * @param gatewayContext
+     * @return
      */
-    void complete(Request request, Response response, Throwable throwable, GatewayContext gatewayContext) {
+    private static Optional<Rule.HystrixConfig> getHystrixConfig(GatewayContext gatewayContext) {
+        //从上下文中获取rule，从rule中获取熔断配置
+        Rule rule = gatewayContext.getRule();
+        //使用stream进行过滤，判断hystrixConfig中的path是否和上下文请求中的path相同，获取第一个，进行返回
+        Optional<Rule.HystrixConfig> hystrixConfig = rule.getHystrixConfigs().stream().filter(c -> c.getPath().equals(gatewayContext.getRequest().getPath())).findFirst();
+        return hystrixConfig;
+    }
+
+
+    /**
+     * todo 有熔断配置时的路由转发
+     * @param gatewayContext
+     * @param hystrixConfig
+     */
+    private void routeWithHystrix(GatewayContext gatewayContext, Optional<Rule.HystrixConfig> hystrixConfig) {
+    }
+    //定义routeWithHystrix方法
+    //使用原生的HystrixCommand，定义一组的key，第一个key时上下文中的唯一id；定义CommandKey，key为上下文的请求中的path
+    //设置线程池大小，从配置中获取
+    //配置CommandProperties，定义隔离策略：线程隔离的方式；设置超时时间，从配置中获取；发生异常时，执行打断；开启超时时间配置
+    //new 原生的HystrixCommand
+    //执行原来的route
+    //定义fallback的获取，设置上下文中的response和上下文的状态；设置完成后执行
+
+
+    /**
+     * 处理异步的向下游发送请求后的结果
+     *
+     * @param request
+     * @param response
+     * @param throwable
+     * @param gatewayContext
+     */
+    void complete(Request request, Response response, Throwable throwable, GatewayContext gatewayContext, Optional<Rule.HystrixConfig> hystrixConfig) {
         //释放资源
         gatewayContext.releaseRequest();
 
@@ -83,7 +139,8 @@ public class RouterFilter implements Filter {
         int configRetryTimes = rule.getRetryConfig().getTimes();
 
         //重试应该在路由转发后，下游服务器返回失败后执行
-        if ((throwable instanceof TimeoutException || throwable instanceof IOException) && currentRetryTimes <= configRetryTimes) {
+        //并且熔断配置不存在时，才进行重试
+        if ((throwable instanceof TimeoutException || throwable instanceof IOException) && currentRetryTimes <= configRetryTimes && !hystrixConfig.isPresent()) {
             doRetry(gatewayContext, currentRetryTimes);
             return;
         }
@@ -116,10 +173,19 @@ public class RouterFilter implements Filter {
         }
     }
 
+    /**
+     * 执行重试
+     *
+     * @param gatewayContext
+     * @param currentRetryTimes
+     */
     private void doRetry(GatewayContext gatewayContext, int currentRetryTimes) {
-        System.out.println("当前重试次数为："+currentRetryTimes);
+        System.out.println("当前重试次数为：" + currentRetryTimes);
+
+        //设置重试次数+1
         gatewayContext.setCurrentRetryTimes(currentRetryTimes + 1);
         try {
+            //重新执行route路由过滤器的过滤动作，重新向下游服务器发送请求
             doFilter(gatewayContext);
         } catch (Exception e) {
             throw new RuntimeException(e);
